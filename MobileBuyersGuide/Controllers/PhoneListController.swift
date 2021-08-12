0805e3f2e3032d264888e49f61981de1c4b6c60d
Atikur Rahman <atik.bd08@gmail.com>
2021-08-12 21:01:19 +0600

    sort results

--- MobileBuyersGuide/Controllers/PhoneListController.swift
+++ MobileBuyersGuide/Controllers/PhoneListController.swift
@@ -25,14 +25,18 @@ class PhoneListController: UIViewController {
     }
     
     private var mobileList: [MobileViewModel] = []
-    private var filteredList: [MobileViewModel] = []
+    private var filteredList: [MobileViewModel] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     // --------------------------
     // MARK: - Action Handlers
     // --------------------------
     
     @objc private func handleDidTapSortBarButtonItem() {
-        print("sort")
+        showSortAlert()
     }
     
     // --------------------------
@@ -59,7 +63,6 @@ class PhoneListController: UIViewController {
             case .success(let mobiles):
                 self?.mobileList = mobiles.map {MobileViewModel(with: $0)}
                 self?.filteredList = self?.mobileList ?? []
-                self?.tableView.reloadData()
                 self?.tableView.isHidden = false
             case .failure(let error):
                 print(error)
@@ -97,6 +100,28 @@ class PhoneListController: UIViewController {
         tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16).isActive = true
+    }
+    
+    // --------------------------
+    // MARK: - Sort
+    // --------------------------
+    
+    private func showSortAlert() {
+        let alertController = UIAlertController(title: "Sort", message: nil, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Price low to high", style: .default, handler: { [weak self] _ in
+            guard let strongSelf = self else { return }
+            strongSelf.filteredList.sort(by: {$0.price < $1.price})
+        }))
+        alertController.addAction(UIAlertAction(title: "Price high to low", style: .default, handler: { [weak self] _ in
+            guard let strongSelf = self else { return }
+            strongSelf.filteredList.sort(by: {$0.price > $1.price})
+        }))
+        alertController.addAction(UIAlertAction(title: "Rating 5-1", style: .default, handler: { [weak self] _ in
+            guard let strongSelf = self else { return }
+            strongSelf.filteredList.sort(by: {$0.rating > $1.rating})
+        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        present(alertController, animated: true)
     }
     
     // --------------------------
@@ -158,11 +183,9 @@ extension PhoneListController: TabControlDelegate {
         if selectedIndex == 1 {
             MobileViewModel.filterFavorites(mobileList: mobileList) { favorites in
                 self.filteredList = favorites
-                self.tableView.reloadData()
             }
         } else {
             self.filteredList = self.mobileList
-            self.tableView.reloadData()
         }
     }
 }
