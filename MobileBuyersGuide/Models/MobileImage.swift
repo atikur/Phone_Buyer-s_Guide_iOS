63829bba2ff15f61111776b3f84ba3c42f175d36
Atikur Rahman <atik.bd08@gmail.com>
2021-08-12 19:51:43 +0600

    add MobileImage struct

---
+++ MobileBuyersGuide/Models/MobileImage.swift
@@ -0,0 +1,13 @@
+//
+//  MobileImage.swift
+//  MobileBuyersGuide
+//
+//  Created by Atikur Rahman on 12/8/21.
+//
+
+import Foundation
+
+struct MobileImage: Codable {
+    let id: String
+    let url: String
+}
