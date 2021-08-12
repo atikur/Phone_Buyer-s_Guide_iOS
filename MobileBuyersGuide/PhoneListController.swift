3de7f68ec482cf54878e69b196a8cdcc2beda909
Atikur Rahman <atik.bd08@gmail.com>
2021-08-12 10:11:33 +0600

    add PhoneListController

---
+++ MobileBuyersGuide/PhoneListController.swift
@@ -0,0 +1,35 @@
+//
+//  PhoneListController.swift
+//  MobileBuyersGuide
+//
+//  Created by Atikur Rahman on 12/8/21.
+//
+
+import UIKit
+
+class PhoneListController: UIViewController {
+    
+    // --------------------------
+    // MARK: - Lifecyle Methods
+    // --------------------------
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+    }
+    
+    // --------------------------
+    // MARK: - Setup UI
+    // --------------------------
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+    }
+    
+    // --------------------------
+    // MARK: - Views
+    // --------------------------
+    
+    
+}
