bbae90d9b5cebc29e51313eea6a63c110119b58a
Atikur Rahman <atik.bd08@gmail.com>
2021-08-12 10:49:51 +0600

    add Color struct

---
+++ MobileBuyersGuide/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  MobileBuyersGuide
+//
+//  Created by Atikur Rahman on 12/8/21.
+//
+
+import UIKit
+
+struct Color {
+    public static let tabFocused = UIColor.black
+    public static let tabUnfocused = UIColor(white: 0.88, alpha: 1.0)
+}
